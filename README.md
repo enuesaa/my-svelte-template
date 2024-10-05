13e98b2664d4878390a31f8eb1dded29abdf944c
nua <enuesaa@gmail.com>
2024-10-06 01:20:58 +0900

    feat: setup svelte app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my-svelte-template
+Template repository of Svelte app.
+
+## Stacks
+- Svelte v5
+- SvelteKit
+- Vite
+- Tailwind CSS
+- Prettier
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './app.css'
+	import Header from './Header.svelte'
+</script>
+
+<svelte:head>
+	<title>my-svelte-template</title>
+</svelte:head>
+
+<Header />
+
+<main class="container mx-auto">
+	<slot />
+</main>

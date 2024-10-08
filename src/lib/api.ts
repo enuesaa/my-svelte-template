1824cfd6d23e83cc44bdd2c4f11c4174732bd67c
nua <enuesaa@gmail.com>
2024-10-08 15:43:43 +0900

    chore: remove unused script

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,36 +1,36 @@
 import { createMutation, createQuery, getQueryClientContext } from '@tanstack/svelte-query'
 
 type DocResponse = {
-  note: string
+	note: string
 }
 export const viewDoc = () =>
-  createQuery({
-    queryKey: ['viewDoc'],
-    queryFn: async (): Promise<DocResponse> => {
-      const res = await fetch(`http://localhost:3000/doc`)
-      return await res.json()
-    }
-  })
+	createQuery({
+		queryKey: ['viewDoc'],
+		queryFn: async (): Promise<DocResponse> => {
+			const res = await fetch(`http://localhost:3000/doc`)
+			return await res.json()
+		},
+	})
 
 type UpdateRequest = {
-  note: string
+	note: string
 }
 export const useUpdateDoc = () => {
-  const queryClient = getQueryClientContext()
+	const queryClient = getQueryClientContext()
 
-  return createMutation({
-    mutationFn: async (body: UpdateRequest) => {
-      const res = await fetch(`http://localhost:3000/doc`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      })
-      await res.json()
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['viewDoc'] })
-    }
-  })
+	return createMutation({
+		mutationFn: async (body: UpdateRequest) => {
+			const res = await fetch(`http://localhost:3000/doc`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			})
+			await res.json()
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['viewDoc'] })
+		},
+	})
 }

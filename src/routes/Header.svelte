b263696d56200ae9cb9386411a49010abc2e81dd
nua <enuesaa@gmail.com>
2025-12-06 15:17:53 +0900

    feat: use `@tailwindcss/vite` and subpath import

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,10 +2,21 @@
 	import { Cloudy } from 'lucide-svelte'
 </script>
 
-<header class="container mx-auto pt-2 pb-4">
+<header>
 	<a href="/">
-		<h1 class="font-zenkaku text-2xl font-bold px-3">
+		<h1>
 			my-svelte-template <Cloudy class="inline" />
 		</h1>
 	</a>
 </header>
+
+<style lang="postcss">
+	@reference "#app.css";
+
+	header {
+		@apply container mx-auto pt-2 pb-4;
+	}
+	h1 {
+		@apply font-zenkaku text-2xl font-bold px-3;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		tailwindcss(),
+	],
 	resolve: {
 		alias: {
 			$lib: path.join(__dirname, './src/lib'),
